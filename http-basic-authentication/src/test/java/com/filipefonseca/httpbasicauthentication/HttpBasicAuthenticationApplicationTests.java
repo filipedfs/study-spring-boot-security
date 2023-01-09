8175d735cca2d22592e81d2d2a51c190a6fdc2c9
Filipe Fonseca <filipe.fonseca@supersim.com.br>
2023-01-09 20:04:07 -0300

    Add http basic authentication initial files

---
+++ http-basic-authentication/src/test/java/com/filipefonseca/httpbasicauthentication/HttpBasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipefonseca.httpbasicauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpBasicAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
